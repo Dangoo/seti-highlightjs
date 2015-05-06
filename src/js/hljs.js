0dd4e5ec66cb7dc8c5fc89f3d81cbe69371587f5
dangoo <daniel.gooss@sinnerschrader.com>
2015-05-06 18:24:28 +0200

    Added styles and examples for CSS and HTML

--- src/js/hljs.js
+++ src/js/hljs.js
@@ -1,5 +1,7 @@
 var hljs = require('../../node_modules/highlight.js/lib/highlight');
 // hljs.registerLanguage('javascript', require('../../node_modules/highlight.js/lib/languages/javascript'));
 hljs.registerLanguage('better-javascript', require('./better-javascript'));
+hljs.registerLanguage('xml', require('../../node_modules/highlight.js/lib/languages/xml'));
+hljs.registerLanguage('css', require('../../node_modules/highlight.js/lib/languages/css'));
 
 hljs.initHighlightingOnLoad();
